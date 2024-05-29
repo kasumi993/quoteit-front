b99e0c446f075549c98ff221d8d38a2c31b8db3b
kasumi993 <khadijag993@gmail.com>
2024-05-29 11:23:06 +0000

    deactivate logout button (auth will be managed in another version)

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -19,7 +19,6 @@
           <div
             v-else
             class="cursor-pointer text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:ring-gray-800"
-            @click="logout"
           >
             Se déconnecter
           </div>
